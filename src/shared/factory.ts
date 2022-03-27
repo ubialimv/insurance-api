22c812ec271087ab835041b8471b19978d28b321
ubialimv <ubiali.marcosvenicius@gmail.com>
2022-03-26 21:21:10 -0300

    feat(shared): add planService to factory

--- src/shared/factory.ts
+++ src/shared/factory.ts
@@ -4,6 +4,7 @@ import CommonRulesEngine from '../domain/engine/common-rules.engine';
 import DisabilityEngine from '../domain/engine/specialized/disability.engine';
 import HomeEngine from '../domain/engine/specialized/home.engine';
 import LifeEngine from '../domain/engine/specialized/life.engine';
+import PlanService from '../domain/plan/plan.service';
 
 const makeCommonRuleEngine = () => new CommonRulesEngine(new BasicEngine());
 
@@ -12,10 +13,19 @@ const makeDisabilityEngine = () => new DisabilityEngine(makeCommonRuleEngine());
 const makeHomeEngine = () => new HomeEngine(makeCommonRuleEngine());
 const makeLifeEngine = () => new LifeEngine(makeCommonRuleEngine());
 
+const makePlanService = () =>
+  new PlanService(
+    makeAutoEngine(),
+    makeDisabilityEngine(),
+    makeHomeEngine(),
+    makeLifeEngine()
+  );
+
 export {
   makeCommonRuleEngine,
   makeAutoEngine,
   makeDisabilityEngine,
   makeHomeEngine,
-  makeLifeEngine
+  makeLifeEngine,
+  makePlanService
 };
